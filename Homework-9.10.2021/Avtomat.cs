531615cf7c263788a0297264b76cff0737fba838
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-10-09 21:08:25 +0400

    Add project files.

---
+++ Homework-9.10.2021/Avtomat.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_9._10._2021
+{
+    class Avtomat
+    {
+        private int _gullesayi;
+
+
+        public int Gullesayi { get => _gullesayi; set => _gullesayi = value; }
+
+        public void MaqaziniTekTekDoldurmaq()
+        {
+            if (_gullesayi != 30)
+            {
+                _gullesayi++;
+                Console.WriteLine($"Gulle Sayi: {_gullesayi}");
+            }
+            else
+            {
+                Console.WriteLine("Gulle sayi maksimumdur");
+            }
+        }
+
+        public void MaqaziniBirbasaDoldurmaq()
+        {
+            _gullesayi = 30;
+            Console.WriteLine($"Gulle Sayi: {_gullesayi}");
+        }
+
+        public void TekliRejim()
+        {
+            if (_gullesayi != 0)
+            {
+                _gullesayi--;
+                Console.WriteLine($"Gulle Sayi: {_gullesayi}");
+            }
+            else
+            {
+                Console.WriteLine("Gulle yoxdur");
+                Console.WriteLine($"Gulle Sayi: {_gullesayi}");
+            }
+        }
+
+        public void AvtomatRejimi()
+        {
+            if (_gullesayi != 0)
+            {
+                _gullesayi = 0;
+                Console.WriteLine($"Gulle Sayi: {_gullesayi}");
+            }
+            else
+            {
+                Console.WriteLine("Gulle yoxdur");
+                Console.WriteLine($"Gulle Sayi: {_gullesayi}");
+            }
+        }
+    }
+}
---
+++ Homework-9.10.2021/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework_9._10._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Avtomat avt = new Avtomat();
+            avt.Gullesayi = 0;
+            Console.WriteLine($"Baslangic Gulle: {avt.Gullesayi}");
+            avt.AvtomatRejimi();
+            avt.MaqaziniTekTekDoldurmaq();
+            avt.MaqaziniBirbasaDoldurmaq();
+            avt.TekliRejim();
+            avt.AvtomatRejimi();
+            avt.TekliRejim();
+        }
+    }
+}
